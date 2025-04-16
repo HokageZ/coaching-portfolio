1e7b9884d4829af69e821a6e2d5fc32e51a79884
Hokage_Z <132206805+HokageZ@users.noreply.github.com>
2025-04-16 18:10:49 +0200

    Add files via upload

---
+++ dist/assets/ScrollToTop-DmyQ0r9R.js
@@ -0,0 +1,2 @@
+import{a as t,r}from"./main-BVHfPotx.js";const s=()=>{const{pathname:o}=t();return r.useEffect(()=>{window.scrollTo({top:0,behavior:"smooth"})},[o]),null};export{s as default};
+//# sourceMappingURL=ScrollToTop-DmyQ0r9R.js.map
