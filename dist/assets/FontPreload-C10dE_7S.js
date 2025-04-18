f1a4568b33050f3ca14021a73e1b941e942a65e4
Hokage_Z <132206805+HokageZ@users.noreply.github.com>
2025-04-18 06:19:21 +0200

    Delete dist directory
    changing the live version

--- dist/assets/FontPreload-C10dE_7S.js
+++
@@ -1,2 +0,0 @@
-import{u as t,r as a}from"./main-BVHfPotx.js";const s=()=>{const{language:e}=t();return a.useEffect(()=>{document.documentElement.style.fontFamily="'Chakra Petch', sans-serif"},[e]),null};export{s as default};
-//# sourceMappingURL=FontPreload-C10dE_7S.js.map
