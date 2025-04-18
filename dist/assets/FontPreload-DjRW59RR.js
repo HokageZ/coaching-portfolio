9e8dd22acd3e479272692fe7d5d6416272a9ed09
Hokage_Z <132206805+HokageZ@users.noreply.github.com>
2025-04-18 08:36:30 +0200

    new build, better spacing, bug fixes

---
+++ dist/assets/FontPreload-DjRW59RR.js
@@ -0,0 +1,2 @@
+import{u as t,r as n}from"./main-DkGt75q1.js";const a=()=>{const{language:e}=t();return n.useEffect(()=>{document.documentElement.lang=e,document.documentElement.dir=e==="ar"?"rtl":"ltr"},[e]),null};export{a as default};
+//# sourceMappingURL=FontPreload-DjRW59RR.js.map
