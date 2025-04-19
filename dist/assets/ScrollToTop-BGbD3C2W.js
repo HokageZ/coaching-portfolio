7eb1c54ac2529be4e7738d06c46a0c21d33ea4d3
Hokage_Z <132206805+HokageZ@users.noreply.github.com>
2025-04-19 09:06:18 +0200

    Delete dist directory

--- dist/assets/ScrollToTop-BGbD3C2W.js
+++
@@ -1,2 +0,0 @@
-import{r as n}from"./main-cs44VMXF.js";const a=()=>(n.useEffect(()=>{window.scrollTo(0,0);const o=()=>{window.location.hash||window.scrollTo(0,0)};return window.addEventListener("hashchange",o),()=>window.removeEventListener("hashchange",o)},[]),null);export{a as default};
-//# sourceMappingURL=ScrollToTop-BGbD3C2W.js.map
