d0ecc1512b5e93e5c70c4b18757bd7800589d0cd
Hokage_Z <132206805+HokageZ@users.noreply.github.com>
2025-04-19 06:59:37 +0200

    Add files via upload

---
+++ dist/assets/FontPreload-qHiX7Xof.js
@@ -0,0 +1,2 @@
+import{u as t,r as n}from"./main-CXYwxsfu.js";const a=()=>{const{language:e}=t();return n.useEffect(()=>{document.documentElement.lang=e,document.documentElement.dir=e==="ar"?"rtl":"ltr"},[e]),null};export{a as default};
+//# sourceMappingURL=FontPreload-qHiX7Xof.js.map
---
+++ dist/assets/ScrollToTop-C4WaidxL.js
@@ -0,0 +1,2 @@
+import{r as n}from"./main-CXYwxsfu.js";const a=()=>(n.useEffect(()=>{window.scrollTo(0,0);const o=()=>{window.location.hash||window.scrollTo(0,0)};return window.addEventListener("hashchange",o),()=>window.removeEventListener("hashchange",o)},[]),null);export{a as default};
+//# sourceMappingURL=ScrollToTop-C4WaidxL.js.map
