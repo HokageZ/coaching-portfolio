1b9021d5ad2c3ae5d6c7fb12b3f86ac0f5e71200
Hokage_Z <132206805+HokageZ@users.noreply.github.com>
2025-04-19 08:57:24 +0200

    Add files via upload

---
+++ dist/assets/FontPreload-C5_zlUyr.js
@@ -0,0 +1,2 @@
+import{u as t,r as n}from"./main-cs44VMXF.js";const a=()=>{const{language:e}=t();return n.useEffect(()=>{document.documentElement.lang=e,document.documentElement.dir=e==="ar"?"rtl":"ltr"},[e]),null};export{a as default};
+//# sourceMappingURL=FontPreload-C5_zlUyr.js.map
