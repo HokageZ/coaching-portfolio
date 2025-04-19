2160593a99133a803bee3ea1b8dadffa69bdee27
Hokage_Z <132206805+HokageZ@users.noreply.github.com>
2025-04-19 06:58:58 +0200

    Delete dist directory

--- dist/assets/ScrollToTop-DPb1zrIR.js
+++
@@ -1,2 +0,0 @@
-import{a as t,r}from"./main-DkGt75q1.js";const s=()=>{const{pathname:o}=t();return r.useEffect(()=>{window.scrollTo({top:0,behavior:"smooth"})},[o]),null};export{s as default};
-//# sourceMappingURL=ScrollToTop-DPb1zrIR.js.map
