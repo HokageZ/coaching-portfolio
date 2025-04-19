6697971bc16c3c5d2d493a202b072fd9f1c4d3f8
Hokage_Z <132206805+HokageZ@users.noreply.github.com>
2025-04-19 07:47:47 +0200

    Delete dist directory

--- dist/assets/FontPreload-na-6HzAH.js
+++
@@ -1,2 +0,0 @@
-import{u as t,r as n}from"./main-B9dDhoaf.js";const a=()=>{const{language:e}=t();return n.useEffect(()=>{document.documentElement.lang=e,document.documentElement.dir=e==="ar"?"rtl":"ltr"},[e]),null};export{a as default};
-//# sourceMappingURL=FontPreload-na-6HzAH.js.map
